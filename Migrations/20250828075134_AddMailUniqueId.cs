3e1c108bd2259ea513375f52fb6f35393a7f95ca
ozgun.dogan <ozgundgn0@gmail.com>
2025-08-31 21:13:56 +0300

    lacks of files

---
+++ Migrations/20250828075134_AddMailUniqueId.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RepairTracking.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddMailUniqueId : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateIndex(
+                name: "mails_pk1",
+                table: "mails",
+                column: "id",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "mails_pk1",
+                table: "mails");
+        }
+    }
+}
